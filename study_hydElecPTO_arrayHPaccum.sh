0cad6ba0c9318122b96978d3bef59288a965dc24
Jeremy Simmons <simmo536@umn.edu>
2024-07-08 16:39:29 -0500

    add SLURM scripts

---
+++ study_hydElecPTO_arrayHPaccum.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=4gb
+#SBATCH -t 8:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=simmo536@umn.edu
+#SBATCH -p msismall
+#SBATCH -o %A_%a.out
+#SBATCH -e %A_%a.err
+
+cd ~/2024-WECarrayPhasing
+module load matlab
+matlab -nodisplay -r \
+"iVar = ${SLURM_ARRAY_TASK_ID}; \
+display(['iVar = ',num2str(iVar)]); \
+SS = ${SS}; \
+display(['SS = ',num2str(SS)]); \
+study_hydElecPTO_arrayHPaccum"
+
+# Commands to use
+# sbatch --export=SS=1 --array=1-675 ~/2024-WECarrayPhasing/study_hydElecPTO_arrayHPaccum.sh
+# dos2unix  study_hydElecPTO_arrayHPaccum.sh
+
